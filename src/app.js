2d4555642e51224e5f803c1f14ffbe7abd02f270
Manjushree1995 <130288335+Manjushree1995@users.noreply.github.com>
2023-05-02 16:49:25 +0530

    module2

--- src/app.js
+++ src/app.js
@@ -1,22 +1,57 @@
-const fs = require("fs");
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
-// Set the directory for views
-app.set("views", path.join(__dirname, "views"));
+// Read the contents of the accounts.json file
+const accountData = fs.readFileSync(
+  path.join(__dirname, "/json/accounts.json"),
+  "utf8"
+);
 
-// Set the view engine to EJS
-app.set("view engine", "ejs");
+// Parse the contents of the file into a JavaScript object
+const accounts = JSON.parse(accountData);
 
-// Serve static assets from the "public" directory
-//app.use(express.static("public"));
+// Set the "public" directory as the static directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Define the route for the root URL path "/"
+// Set the "views" directory for the "ejs" templates
+app.set("views", path.join(__dirname, "views"));
+
+// Set the view engine to "ejs"
+app.set("view engine", "ejs");
+
+const userData = fs.readFileSync(
+  path.join(__dirname, "/json/users.json"),
+  "utf8"
+);
+const users = JSON.parse(userData);
+
+//Define the route for the root URL path "/"
 app.get("/", (req, res) => {
   // Render the "index" view and pass an object with a "title" key-value pair
-  res.render("index", { title: "Index" });
+  // and a new key-value pair "accounts: accounts"
+  res.render("index", { title: "Account Summary", accounts: accounts });
+});
+
+app.get("/savings", (req, res) => {
+  res.render("account", {
+    title: "Account Summary",
+    account: accounts.savings,
+  });
+});
+app.get("/checking", (req, res) => {
+  res.render("account", {
+    title: "Account Summary",
+    account: accounts.checking,
+  });
+});
+app.get("/credit", (req, res) => {
+  res.render("account", { title: "Account Summary", account: accounts.credit });
+});
+
+app.get("/profile", (req, res) => {
+  res.render("profile", { user: users[0] });
 });
 
 // Start the server
