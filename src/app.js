e99034a4e08aeaf4bc40a6a5f350fa17a054f405
Manjushree1995 <130288335+Manjushree1995@users.noreply.github.com>
2023-05-02 17:33:57 +0530

    module5

--- src/app.js
+++ src/app.js
@@ -1,90 +1,26 @@
-const express = require("express");
-const app = express();
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
 
-// Read the contents of the accounts.json file
-const accountData = fs.readFileSync(
-  path.join(__dirname, "/json/accounts.json"),
-  "utf8"
-);
+const express = require("express");
+const app = new express();
 
-// Parse the contents of the file into a JavaScript object
-const accounts = JSON.parse(accountData);
-
-// Set the "public" directory as the static directory
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
-
-// Set the "views" directory for the "ejs" templates
-app.set("views", path.join(__dirname, "views"));
-
-// Set the view engine to "ejs"
+const { accounts, users } = require("./data.js");
+const accountRoutes = require("./routes/accounts.js");
+const servicesRoutes = require("./routes/services.js");
+app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
-const userData = fs.readFileSync(
-  path.join(__dirname, "/json/users.json"),
-  "utf8"
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) =>
+  res.render("index", { title: "Account Summary", accounts: accounts })
 );
-const users = JSON.parse(userData);
 
-const { writeJSON } = require("./data.js");
+app.use("/account", accountRoutes);
+app.use("/services", servicesRoutes);
+app.get("/profile", (req, res) => res.render("profile", { user: users[0] }));
 
-//Define the route for the root URL path "/"
-app.get("/", (req, res) => {
-  // Render the "index" view and pass an object with a "title" key-value pair
-  // and a new key-value pair "accounts: accounts"
-  res.render("index", { title: "Account Summary", accounts: accounts });
-});
-
-app.get("/savings", (req, res) => {
-  res.render("account", {
-    title: "Account Summary",
-    account: accounts.savings,
-  });
-});
-app.get("/checking", (req, res) => {
-  res.render("account", {
-    title: "Account Summary",
-    account: accounts.checking,
-  });
-});
-app.get("/credit", (req, res) => {
-  res.render("account", { title: "Account Summary", account: accounts.credit });
-});
-app.get("/transfer", (req, res) => {
-  res.render("transfer", {
-    title: "Account Summary",
-    account: accounts.credit,
-  });
-});
-
-app.get("/profile", (req, res) => {
-  res.render("profile", { user: users[0] });
-});
-
-app.get("/transfer", (req, res) => res.render("transfer"));
-app.post("/transfer", (req, res) => {
-  accounts[req.body.from].balance -= req.body.amount;
-  accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-  writeJSON();
-  res.render("transfer", { message: "Transfer Completed" });
-});
-
-app.get("/payment", (req, res) =>
-  res.render("payment", { account: accounts.credit })
-);
-app.post("/payment", (req, res) => {
-  accounts.credit.balance -= req.body.amount;
-  accounts.credit.available += parseInt(req.body.amount);
-  writeJSON();
-  res.render("payment", {
-    message: "Payment Successful",
-    account: accounts.credit,
-  });
-});
-
-// Start the server
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
+  console.log("PS Project Running on port 3000!");
 });
